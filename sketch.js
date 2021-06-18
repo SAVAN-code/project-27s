d0c32eebcd23d02bfa46b7eb3d6fd389a12fde31
SAVAN-code <83593623+SAVAN-code@users.noreply.github.com>
2021-06-18 07:24:28 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(800,400);
+  var stick = createSprite(400,70,500,30);
+  stick.shapeColor = "blue";
+
+ var ball = createSprite()  
+}
+
+function draw() {
+  background("gray");  
+  drawSprites();
+}
